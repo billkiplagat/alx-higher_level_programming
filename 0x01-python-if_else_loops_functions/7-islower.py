5952dbc096b77231170a420e7b60f9590ff1747a
billkiplagat <bill.kiplagat.7@gmail.com>
2023-06-06 21:52:21 +0300

    updated uppercase function

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
+    # To obtain the ASCII value of the letter
     asi = ord(c)
     if 97 <= asi <= 122:
         return True
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    converted_text = ""
+    for char in str:
+        ascii_val = ord(char)
+        if 97 <= ascii_val <= 122:
+            # chr() returns a string representing a character whose
+            # Unicode code point is the specified integer
+            converted_char = chr(ascii_val - 32)
+        else:
+            converted_char = char
+        converted_text += converted_char
+    print("{}".format(converted_text))
