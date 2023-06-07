c6b2abe0bfb194cf058e20f41c659d32d5f33bd2
billkiplagat <bill.kiplagat.7@gmail.com>
2023-06-07 10:58:43 +0300

    Printing upper and lower

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        print("{}".format(chr(i)), end="")
+        print("{:c}".format(i), end="")
     else:
-        print("{}".format(chr(i).upper()), end="")
+        print("{:c}".format(i - 32), end="")
