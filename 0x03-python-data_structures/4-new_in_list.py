ed8574eb1921144e097d69f0fc441620f49ec24a
billkiplagat <bill.kiplagat.7@gmail.com>
2023-06-10 11:41:12 +0300

    updated return

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,14 @@
 def new_in_list(my_list, idx, element):
     if my_list is None:
         return
-    modified_list = []
-    for i in my_list:
-        if i == my_list[idx]:
-            modified_list.append(element)
-        else:
-            modified_list.append(i)
-    return modified_list
+    if idx < 0:
+        return my_list
+    if idx < len(my_list):
+        modified_list = []
+        for i in my_list:
+            if i == my_list[idx]:
+                modified_list.append(element)
+            else:
+                modified_list.append(i)
+        return modified_list
+    return my_list
