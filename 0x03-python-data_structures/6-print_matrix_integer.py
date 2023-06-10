055d51970333443b3794d1c8205f948bc55cfc81
billkiplagat <bill.kiplagat.7@gmail.com>
2023-06-10 11:45:00 +0300

    updated return matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return
     for row in matrix:
         # enumerate function to iterate over the elements in each row
         for row_index, value in enumerate(row):
