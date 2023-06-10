fea9280c950164071d7cb32cdfd0781e8f1cf65b
billkiplagat <bill.kiplagat.7@gmail.com>
2023-06-10 11:26:24 +0300

    updated condition

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    list_len = len(my_list)
     if idx < 0:
         return None
-    elif idx > list_len:
-        return None
-    else:
+    if idx < len(my_list):
         return my_list[idx]
+    else:
+        return None
