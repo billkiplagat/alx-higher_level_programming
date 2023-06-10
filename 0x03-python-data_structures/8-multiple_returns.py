bb057eb89fbfa4018653c428e3bbffa283903539
billkiplagat <bill.kiplagat.7@gmail.com>
2023-06-10 12:43:01 +0300

    Updated condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if sentence is None:
-        first_char = None
+    if length > 0:
+        # Meaning we have a sentence
+        first_chr = sentence[0]
     else:
-        first_char = sentence[0]
-    new_tuple = (length, first_char)
-    return new_tuple
+        first_chr = None
+    return length, first_chr
