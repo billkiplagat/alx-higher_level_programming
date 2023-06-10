d1b4bbea89d1f21a29a69ddddcb9b73f9f9e40bc
billkiplagat <bill.kiplagat.7@gmail.com>
2023-06-10 12:36:53 +0300

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0]
     if sentence is None:
         first_char = None
+    else:
+        first_char = sentence[0]
     new_tuple = (length, first_char)
     return new_tuple
