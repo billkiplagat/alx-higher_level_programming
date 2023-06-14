81e96b2200a1186184e0a749a360e34620932b5b
billkiplagat <bill.kiplagat.7@gmail.com>
2023-06-14 17:02:32 +0300

    updated max argument

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    max_key = max(a_dictionary, key=lambda k: a_dictionary[k])
+    max_key = max(a_dictionary, key=a_dictionary.get)
     return max_key
