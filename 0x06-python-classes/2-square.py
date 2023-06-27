76aa43710982f2b97cc231aa291330204a748b7a
billkiplagat <bill.kiplagat.7@gmail.com>
2023-06-27 14:20:24 +0300

    updates type

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ New class Square
 class Square:
     """ Defines a Square """
     def __init__(self, size=0):
-        if type(size != int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
