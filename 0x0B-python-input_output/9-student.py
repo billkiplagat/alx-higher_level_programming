f63102f4c40832b2d0216605330ae258e6e42c4d
billkiplagat <bill.kiplagat.7@gmail.com>
2023-07-11 15:15:15 +0300

    Updated Student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a class Student"""
+
+
+class Student:
+    """Initializes a new Student
+            """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Gets a dictionary representation of the Student"""
+        return self.__dict__
