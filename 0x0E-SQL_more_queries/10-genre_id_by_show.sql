8cc5f44f374a94d2d42a7f78a708094aa74b5c4f
billkiplagat <bill.kiplagat.7@gmail.com>
2023-08-16 21:51:57 +0300

    Updated Inner join

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- lists all shows contained in hbtn_0d_tvshows that
+-- have at least one genre linked
 SELECT s.title, g.genre_id
-FROM tv_shows AS s INNER JOIN tv_show_genres AS g
+FROM tv_shows s INNER JOIN tv_show_genres g
 ON s.id = g.show_id
 ORDER BY s.title, g.genre_id;
--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,4 +5,3 @@ SELECT t.title, g.genre_id
 FROM tv_shows AS t LEFT JOIN tv_show_genres AS g
 ON t.id = g.show_id
 ORDER BY t.title, g.genre_id;
-
