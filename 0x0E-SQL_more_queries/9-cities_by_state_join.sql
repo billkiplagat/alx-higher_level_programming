b1d8f61b34b7ad05d41cdd98919fce92a9097816
billkiplagat <bill.kiplagat.7@gmail.com>
2023-08-16 14:57:02 +0300

    Updated Inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- lists all cities contained in the database with state names
 -- We create inner joins based on what the tables have in common
 -- E.g sql is selecting all rows that have matching state_id
-SELECT cities.id, cities.name, states.name
-FROM hbtn_0d_usa.cities INNER JOIN hbtn_0d_usa.states
-    ON cities.state_id = states.id
-ORDER BY cities.id;
+SELECT c.id, c.name, s.name
+FROM cities AS c INNER JOIN states AS s
+    ON c.state_id = s.id
+ORDER BY c.id;
