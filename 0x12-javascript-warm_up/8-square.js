d88dd51bc7df38a8b7c39de3996f65b31b52c268
billkiplagat <bill.kiplagat.7@gmail.com>
2023-09-11 15:54:43 +0300

    Updated condition

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const arg1 = process.argv.slice(2);
+const intVal = parseInt(arg1);
+if (!isNaN(intVal)) {
+  for (let i = 0; i < intVal; i++) {
+    let row = '';
+    for (let j = 0; j < intVal; j++) {
+      row += 'x';
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+if (process.argv.length !== 4) {
+  console.log('NaN');
+  process.exit(1);
+}
+const num1 = parseFloat(process.argv[2]);
+const num2 = parseFloat(process.argv[3]);
+if (isNaN(num1) || isNaN(num2)) {
+  console.log('Invalid input. Both arguments must be numbers.');
+  process.exit(1);
+}
+function add (a, b) {
+  return a + b;
+}
+const result = add(num1, num2);
+console.log(result);
