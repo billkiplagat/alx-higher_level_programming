4e5a26b230de8ce9935d53bba5692e0d47d3d7a2
billkiplagat <bill.kiplagat.7@gmail.com>
2023-09-11 14:05:08 +0300

    removed length

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 /* node is always the first argument */
-if (process.argv && process.argv.length > 2) {
-  console.log('Arguments found');
-} else if (process.argv && process.argv.length === 3) {
-  console.log('Argument found');
+if (process.argv[2] !== undefined) {
+  if (process.argv[3] !== undefined) {
+    console.log('Arguments found');
+  } else {
+    console.log('Argument found');
+  }
 } else {
   console.log('No argument');
 }
