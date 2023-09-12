5b4c8ad20b082d2039c2b59ccc912404dd5bf4d6
billkiplagat <bill.kiplagat.7@gmail.com>
2023-09-12 21:27:50 +0300

    Completed Mandatory

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return function convertToBase (number) {
-    if (number < base) {
-      return number.toString();
-    } else {
-      return convertToBase(Math.floor(number / base)) + (number % base).toString();
-    }
+  return function(n){
+    return (n.toString(base));
   };
 };
