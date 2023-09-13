95b2077772461a90722395d68427830cfbef5d2d
billkiplagat <bill.kiplagat.7@gmail.com>
2023-09-13 11:40:27 +0300

    Updated factorial function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+function computeFactorial (number) {
+  if (isNaN(number) || number < 0) {
+    return 1;
+  }
+  if (number === 0) {
+    return 1;
+  }
+  return number * computeFactorial(number - 1);
+}
+
+const input = process.argv[2];
+const number = parseInt(input, 10);
+
+const factorial = computeFactorial(number);
+
+console.log(`${factorial}`);
