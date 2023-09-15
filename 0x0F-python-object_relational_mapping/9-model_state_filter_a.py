c70f3d08b5191f696e640559895aa26a5bdef2e6
billkiplagat <bill.kiplagat.7@gmail.com>
2023-09-15 10:20:01 +0300

    Updated query

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+lists all State objects that contain the letter a from the database
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
