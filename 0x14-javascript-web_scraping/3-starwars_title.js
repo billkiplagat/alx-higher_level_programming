c445998948e4a6fe2eb6e0a54061cda2b09179ca
billkiplagat <bill.kiplagat.7@gmail.com>
2023-10-24 12:20:08 +0300

    updated semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/';
-request(url + movieId, function (error, body) {
+request(url + movieId, function (error, respones, body) {
   if (error) {
     console.log(error);
   } else {
